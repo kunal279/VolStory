50043f7e8fead6ed34d3229e45b39a83aab26d97
kunal279 <kunal270692@gmail.com>
2018-11-05 13:07:42 +0530

    git second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,31 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+//import { HomePage } from '../pages/home/home';
+import { SignUpPage } from '../pages/sign-up/sign-up';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = SignUpPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, splashScreen: SplashScreen, private statusBar:StatusBar) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      
       statusBar.styleDefault();
+      this.statusBar.overlaysWebView(true);
+      this.statusBar.backgroundColorByHexString('#03756c');
       splashScreen.hide();
+           
+
+      
+
     });
   }
+
 }
 
